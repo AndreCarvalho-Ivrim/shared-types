6d3c2d37d1ae87aa8f072c9d2def8933d8380524
Mateus Brandão <mateusfleria@gmail.com>
2023-08-03 20:47:08 -0300

    feat: Atualizando FlowEntitySchemaTypes

--- workflow.entities.type.ts
+++ workflow.entities.type.ts
@@ -9,7 +9,7 @@ export interface FlowEntitySchemaInfo{
   type: FlowEntitySchemaTypes,
   label: string,
   placeholder?: string,
-  mask?: 'email' | 'number' | 'money' | 'cpf' | 'cnpj' | 'cpf-cnpj' | 'cep' | 'phone' | 'url',
+  mask?: 'email' | 'cpf' | 'cnpj' | 'cpf-cnpj' | 'cep' | 'phone' | 'url',
   required: boolean,
   unique?: boolean
 }
