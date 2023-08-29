300948f4d3f274e0ee7b2d5b10f68be7d74adb95
Mateus Brandão <mateusfleria@gmail.com>
2023-08-29 10:44:19 -0300

    refector:

--- workflow.config.type.ts
+++ workflow.config.type.ts
@@ -36,8 +36,10 @@ export interface WorkflowConfigAutocomplete{
 export interface WorkflowConfigObserverFnType{
   name: string,
   /** EVENTS -> available names on type event
-   * @prevent-stock-out: Evento de revalidação de estoque na estrutura do WF Duzani
+   * @revalidate-when-updated-product: Evento de revalidação de estoque na estrutura do WF Duzani
    */
+
+
   type: 'append' | 'backup' | 'event',
   execute: 'before' | 'after',
   condition?: string,
