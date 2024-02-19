9c2b19665503d6056ddbc7dade96a99ffbe3a560
Mateus Brandão <mateusfleria@gmail.com>
2024-02-19 12:22:17 -0300

    feat: adicionando tipagem de FlowDataNotesType

---
+++ flow_data_notes.type.ts
@@ -0,0 +1,22 @@
+export interface FlowDataNote{
+  attachments?: Array<{
+    url: string,
+    id: string,
+    name: string
+  }>,
+  content: string,
+  author: string,
+  answers?: FlowDataNote[],
+  created_at: Date,
+  updated_at: Date,
+  /** Id dos usuários que curtiram */
+  likes?: string[],
+  /** Id dos usuários que deram deslike */
+  unlikes?: string[]
+}
+export interface FlowDataNotesType{
+  _id: string,
+  flow_id: string,
+  flow_data_id: string,
+  notes: FlowDataNote[]
+}
