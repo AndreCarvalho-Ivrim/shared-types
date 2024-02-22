e3e6ab03a1ddb22fbba8eaf38fd3fdc0302d06f9
Mateus Brandão <mateusfleria@gmail.com>
2024-02-22 12:26:09 -0300

    feat: Lidando com verificação de existencia no strc com string = false

--- utils/check-string-conditional.ts
+++ utils/check-string-conditional.ts
@@ -89,7 +89,7 @@ export const checkStringConditional = (strConditional: string, datas: Record<str
     //#endregion HANDLE POSSIBLE BOOL
 
     if (val_2 === '!!') return !!val_1;
-    if (val_2 === '!') return !val_1;
+    if (val_2 === '!') return !val_1 || val_1 === 'false';
 
     if (Array.isArray(val_1)) throw new Error(
       'O primeiro valor não pode ser uma lista'
