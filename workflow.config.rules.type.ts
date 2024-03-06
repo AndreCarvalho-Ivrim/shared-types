35f52c84f1097e9d824917a7cc83c1c9eb649738
Mateus Brandão <mateusfleria@gmail.com>
2024-03-06 14:51:16 -0300

    feat: Adicionado decomposto tipagens internas de RuleConverge, WorkflowConfigRuleConvergeReplacer, WorkflowConfigRuleConvergeEffect

--- workflow.config.rules.type.ts
+++ workflow.config.rules.type.ts
@@ -17,23 +17,25 @@ export interface WorkflowConfigRuleConvergence extends WorkflowConfigRulesBase{
      * Se passar duas strings(string: string) fará o overwrite dos dados de origem. Caso queira um comportamento \
      * especifico, abra o objeto e selecione o tipo adequado.
      */
-    replacers: Record<string, string | {
-      ref: string,
-      /**
-       * - [merge] irá mesclar os campos, caso seja objeto ou array(ambos precisam ser do mesmo tipo, ou undefined) \
-       * e caso no máximo um seja undefined.
-       * - [overwrite] sobrescrever os dados de origem
-       * - [over-left] só sobrescreverá se os dados da esquerda não estiverem preenchidos
-       * - [over-right] só sobrescreverá se os dados da direita estiverem preenchidos
-       */
-      type: 'merge' | 'overwrite' | 'over-left' | 'over-right'
-    }>,
-    effects?: Array<{
-      /** - (default) always */
-      only?: 'always' | 'converged' | 'diverged',
-      append_values: Record<string, any>
-    }>
+    replacers: Record<string, string | WorkflowConfigRuleConvergeReplacer>,
+    effects?: WorkflowConfigRuleConvergeEffect[]
   }
+}
+export interface WorkflowConfigRuleConvergeReplacer{
+  ref: string,
+  /**
+   * - [merge] irá mesclar os campos, caso seja objeto ou array(ambos precisam ser do mesmo tipo, ou undefined) \
+   * e caso no máximo um seja undefined.
+   * - [overwrite] sobrescrever os dados de origem
+   * - [over-left] só sobrescreverá se os dados da esquerda não estiverem preenchidos
+   * - [over-right] só sobrescreverá se os dados da direita estiverem preenchidos
+   */
+  type: 'merge' | 'overwrite' | 'over-left' | 'over-right'
+}
+export interface WorkflowConfigRuleConvergeEffect{
+  /** - (default) always */
+  only?: 'always' | 'converged' | 'diverged',
+  append_values: Record<string, any>
 }
 /** Regra de formatação de dados do flow-data */
 export interface WorkflowConfigRuleFormatters extends WorkflowConfigRulesBase{
