9cfa2b23a86e8572c1e32c7b3a9af413386f074d
Franciel Santos Silva <francielrpm@gmail.com>
2024-11-04 15:40:25 -0300

    feat: adicionado _id no flow_log.type

--- flow_log.type.ts
+++ flow_log.type.ts
@@ -5,10 +5,11 @@ export interface FlowDataLog {
   status: FlowLogStatus;
   description: string;
   amount?: number;
-  data?: Record<string, string>
+  data?: Record<string, any>
   created_at: Date;
 }
 export interface FlowLogType{
+  _id?: string,
   flow_id: string;
   title?: string;
   status: FlowLogStatus;
@@ -17,3 +18,32 @@ export interface FlowLogType{
   started_at: Date;
   finished_at: Date;
 }
+export interface CardFlowLog {
+  type: FlowLogTypes,
+  title: string,
+  description: string
+}
+
+export const availableFlowLogTypes : CardFlowLog[] = [
+  {
+    type: 'webhook',
+    title: 'Webhook',
+    description: 'Logs de processos automatizados.'
+  }, {
+    type: 'public-routes',
+    title: 'Rotas Públicas',
+    description: 'Logs de rotas públicas.'
+  }, {
+    type: 'routine-event',
+    title: 'Rotina de Eventos',
+    description: 'Logs de rotinas de eventos.'
+  }, {
+    type: 'widget-event',
+    title: 'Worker Thread',
+    description: 'Logs de tarefas em segundo plano.'
+  }, {
+    type: 'observer-event',
+    title: 'Observador de Eventos',
+    description: 'Logs de observadores de eventos.'
+  }
+];
