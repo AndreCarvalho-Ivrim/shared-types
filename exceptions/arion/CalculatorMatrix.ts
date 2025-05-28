3c2fbcd06f885abd99812181071780c7495a1c44
Mateus Brandão <mateusfleria@gmail.com>
2025-05-28 20:15:41 -0300

    feat: Removendo tipagem ContractDeadline

--- exceptions/arion/CalculatorMatrix.ts
+++ exceptions/arion/CalculatorMatrix.ts
@@ -43,7 +43,6 @@ export interface ICircuit {
   margin_recurring?: number;
   margin_eventual?: number;
 }
-export type ContractDeadline = 12 | 24 | 36 | 48 | 60 | 72;
 export type CustomerProfile = 'Operadora' | 'Corporativo';
 export type CalculatorMatrixUF = 'AC' | 'AL' | 'AP' | 'AM' | 'BA' | 'CE' | 'DF' | 'ES' | 'GO' | 'MA' | 'MT' | 'MS' | 'MG' | 'PA' | 'PB' | 'PR' | 'PE' | 'PI' | 'RJ' | 'RN' | 'RS' | 'RO' | 'RR' | 'SC' | 'SP' | 'SE' | 'TO';
 export type CalculatorMatrixICMSByUF = Record<CalculatorMatrixUF, any>;
@@ -83,7 +82,7 @@ export interface ICalculatorMatrixData {
 export interface ICalculateTotalParams {
   totalRecurring: number;
   totalEventual: number;
-  contractDeadline: ContractDeadline;
+  contractDeadline: number;
   marginRecurringTotal: number;
   marginEventualTotal: number;
   recurringSalesPriceGrossTotal: number;
