5703a44dc0c0b04c6ad9a8456dce8a4bdba136b1
Mateus Brandão <mateusfleria@gmail.com>
2025-07-09 15:39:28 -0300

    refector: Alterado tipagem do filter

--- step.item.view.type.ts
+++ step.item.view.type.ts
@@ -127,7 +127,7 @@ export interface StepViewHorizontalTableType extends Omit<StepViewGroupTableType
     field: string,
     order: 'asc' | 'desc'
   }
-  filter?: { condition: string }[]
+  filter?: { condition: string }
 }
 export interface StepViewTasksType extends StepViewBaseType{
   /**
