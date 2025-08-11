5932980729641b8c43aec2bd83d21f657525398d
Mateus Brandão <mateusfleria@gmail.com>
2025-08-11 09:32:48 -0300

    refector: Removido process.env.REACT_APP...

--- utils/routes.ts
+++ utils/routes.ts
@@ -220,19 +220,8 @@ export const handleRegexUrl = (url: AvailableRegexUrls, token?: string): string 
 }
 export const getDomain = (application: 'hub' | 'isac' | 'isac_back' |  'hub_back', removeLastSlash = false) => {
   let urls = { hub: '', isac: '', isac_back: '', hub_back: '' }
+
   try {
-    // @ts-ignore
-    const WORKFLOW_MODULE = process.env.REACT_APP_WORKFLOW_MODULAR;
-    urls.isac = WORKFLOW_MODULE!;
-    // @ts-ignore
-    urls.isac_back = process.env.REACT_APP_API_WF_URL
-    // @ts-ignore
-    urls.hub_back = process.env.REACT_APP_BASE_URL
-  } catch (e) { }
-  try {
-    // @ts-ignore
-    const PORTAL = import.meta.env.VITE_PORTAL_URL;
-    urls.hub = ''; // PORTAL!; -- portal descontinuado
     // @ts-ignore
     urls.isac_back = import.meta.env.VITE_BASE_URL
     // @ts-ignore
@@ -269,24 +258,7 @@ export const getSupportKeys = () => {
     support.steps["approval-test"] = import.meta.env.VITE_SUPPORT_APPROVAL_TEST;
     // @ts-ignore
     support.steps["called-closed"] = import.meta.env.VITE_SUPPORT_CALLED_CLOSED;
-  } catch (e) {
-    try {
-      // @ts-ignore
-      support.flow_id = process.env.REACT_APP_SUPPORT_FLOW_ID;
-      // @ts-ignore
-      support.steps["open-request-called"] = process.env.REACT_APP_SUPPORT_OPEN_REQUEST_CALLED;
-      // @ts-ignore
-      support.steps["internal-approval"] = process.env.REACT_APP_SUPPORT_INTERNAL_APPROVAL;
-      // @ts-ignore
-      support.steps["in-progress"] = process.env.REACT_APP_SUPPORT_IN_PROGRESS;
-      // @ts-ignore
-      support.steps["internal-test"] = process.env.REACT_APP_SUPPORT_INTERNAL_TEST;
-      // @ts-ignore
-      support.steps["approval-test"] = process.env.REACT_APP_SUPPORT_APPROVAL_TEST;
-      // @ts-ignore
-      support.steps["called-closed"] = process.env.REACT_APP_SUPPORT_CALLED_CLOSED;
-    } catch (e) { }
-  }
+  } catch (e) { }
   
   return support
 }
