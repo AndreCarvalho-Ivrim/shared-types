9e95d4b89e0e8dd26e2c5b2acf238cd170fc6e15
Mateus Brandão <mateusfleria@gmail.com>
2025-08-21 13:19:23 -0300

    feat: Adicionando tipagem em isac-flow-stats

---
+++ exceptions/isac/isac-flow-stats.type.ts
@@ -0,0 +1,26 @@
+export interface AnalysisGroupColumn{
+  /**
+   * Key do flow-data ou utilitários:
+   * 
+   * - \@steps: Gera uma coluna por etapa
+   * - \@progress: Se a análise gera um progresso acessa o valor
+   */
+  key: string,
+  /**
+   * No caso da key = \@steps é necessário utilizar o valor \@step-title ou \@step-name
+   */
+  name: string
+}
+interface AnalysisGroupBase{
+  key: string,
+  title: string,
+  columns: AnalysisGroupColumn[]
+}
+
+interface AnalysisGroupQuantityPerStep extends AnalysisGroupBase{
+  mode: 'quantity-per-step',
+  groupBy?: string[],
+  completed_steps: string[]
+}
+
+export type AnalysisGroupType = AnalysisGroupQuantityPerStep;
