2b35142fd2ba4dca48f9d733482ac45a53a687e6
Mateus Brandão <mateusfleria@gmail.com>
2026-01-09 09:13:05 -0300

    feat: Adicionado additionals nos grupos do menu e a opção outhers

--- workflow.config.type.ts
+++ workflow.config.type.ts
@@ -1175,15 +1175,18 @@ export interface WorkflowConfigType {
      * Id de todas as configurações que podem gerar botões para acesso no menu. \
      * Abaixo terá um exemplo do prefixo de uma view, entidade, entre outros:
      * 
-     * - view:     view-mode--vm-slug
-     * - entity:   entity--en-id
-     * - shortcut: shortcut--id
+     * - view:      view-mode--vm-slug
+     * - entity:    entity--en-id
+     * - shortcut:  shortcut--id
+     * - exception: exception--id
      */
     ordenation?: string[],
     shortcuts?: WorkflowMenuShortcut[]
-    groups?: Partial<Record<'flow_entities' | 'exception_views' | 'shortcuts', {
+    groups?: Partial<Record<'flow_entities' | 'exception_views' | 'shortcuts' | 'outhers', {
       title: string,
       icon?: AvailableIcons,
+      additionals?: string[],
+      /** É obrigatório informar o only caso seja outhers */
       only?: string[]
     }>>
   },
