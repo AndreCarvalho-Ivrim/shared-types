e154c7049320d25038b142c0a7878ff13a7b97fb
Franciel Santos Silva <francielyorke@gmail.com>
2026-01-16 11:31:25 -0300

    feat: adicionado exclude no group de entidades

--- workflow.config.type.ts
+++ workflow.config.type.ts
@@ -1367,7 +1367,8 @@ export interface WorkflowConfigMenuGroupType{
   icon?: AvailableIcons,
   additionals?: string[],
   /** É obrigatório informar o only caso seja outhers */
-  only?: string[]
+  only?: string[],
+  exclude?: string[]
 }
 interface WorkflowConfigFlowAlertFnBase{
   listening?: { condition: string },
