ff9a191a9abcbac45afb751aaebaa398bd913840
Mateus Brandão <mateusfleria@gmail.com>
2026-02-25 16:49:30 -0300

    feat: Adicionado actions no flow-entity

--- workflow.entities.type.ts
+++ workflow.entities.type.ts
@@ -1,6 +1,6 @@
 import { AvailableIcons } from "./icon.type";
 import { StepViewAttrMaskType } from "./step.item.view.type";
-import { WorkflowConfigFilterRefType, WorkflowConfigFilterType, WorkflowConfigObserverFnType, WorkflowViewModeDashboardModuleBlock } from "./workflow.config.type";
+import { WorkflowConfigActionsType, WorkflowConfigFilterRefType, WorkflowConfigFilterType, WorkflowConfigObserverFnType, WorkflowViewModeDashboardModuleBlock } from "./workflow.config.type";
 
 interface FlowEntityDataFilters {
   "_key"?: string,
@@ -281,6 +281,7 @@ export interface FlowEntityInfo{
   is_public?: boolean,
   styles_form?: FlowEntityStylesForm,
   relations?: FlowEntityRelations[]
+  actions?: WorkflowConfigActionsType[]
 }
 
 export interface FlowEntityStylesFormGroup{
