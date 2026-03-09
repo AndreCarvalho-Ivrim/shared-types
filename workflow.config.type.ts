2678c971cf5c9768e457184542c3e852c0354a6e
Franciel Santos Silva <francielyorke@gmail.com>
2026-03-09 09:19:42 -0300

    feat: adicionado hidden no rules do campos das etapas

--- workflow.config.type.ts
+++ workflow.config.type.ts
@@ -493,7 +493,7 @@ export interface ConfigViewModeColumnsType {
    */
   id: '@user' | '@owners' | 'created_at' | 'step' | '@title-and-subtitle:id_1,id_2' | string,
   name: string,
-  type: IntegrationExcelColumnTypeType | 'tasks' | 'multi-progress',
+  type: IntegrationExcelColumnTypeType | 'tasks' | 'multi-progress' | '@link',
   /**
    * Serve para fazer correspondência entre valores, exemplo, em um campo boolean:
    * 
