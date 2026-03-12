61e1a1a6f82c78c0dffd959531287cab23ed8231
Franciel Santos Silva <francielyorke@gmail.com>
2026-03-12 11:03:00 -0300

    feat: ajuste no calculo da matriz da Arion

--- exceptions/arion/CalculatorMatrix.ts
+++ exceptions/arion/CalculatorMatrix.ts
@@ -78,6 +78,17 @@ export interface ICircuit {
   network?: { id: string },
   special_project_with_adjustment?: boolean
 }
+export interface IValidedSingleCircuitResult {
+  hiringCosts: ICalculateHiringCostsResult,
+  recurringSalesPrice: ICalculateRecurringSalesPriceResul,
+  eventualSalePriceOrInstallationFee: ICalculateBaseResult,
+  margin: ICalculateMarginResult,
+  marginRecurring: number,
+  marginEventual: number,
+  linkQtd: number,
+  monthly_fee_margin_of_error: number,
+  installation_fee_margin_of_error: number,
+}
 export type CustomerProfile = 'Operadora' | 'Corporativo';
 export type CalculatorMatrixUF = 'AC' | 'AL' | 'AP' | 'AM' | 'BA' | 'CE' | 'DF' | 'ES' | 'GO' | 'MA' | 'MT' | 'MS' | 'MG' | 'PA' | 'PB' | 'PR' | 'PE' | 'PI' | 'RJ' | 'RN' | 'RS' | 'RO' | 'RR' | 'SC' | 'SP' | 'SE' | 'TO';
 export type CalculatorMatrixICMSByUF = Record<CalculatorMatrixUF, any>;
