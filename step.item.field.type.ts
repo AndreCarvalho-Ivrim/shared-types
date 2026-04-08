bb157a865dea03e848a5be98eae171aa48f9c221
vitorbelarmino <vitor.belarmino@hotmail.com>
2026-04-08 10:29:43 -0300

    feat: Adiciona nova máscara de atributo 'opercase' ao tipo StepItemAttrMaskType

--- step.item.field.type.ts
+++ step.item.field.type.ts
@@ -16,7 +16,7 @@ export const stepItemAttrTypeFormatted : Record<StepItemAttrTypeType,string> = {
   custom: 'Customizado'
 };
 
-export type StepItemAttrMaskType = 'email' | 'number' | 'money' | 'cpf' | 'cnpj' | 'cpf-cnpj' | 'cep' | 'phone';
+export type StepItemAttrMaskType = 'email' | 'number' | 'money' | 'cpf' | 'cnpj' | 'cpf-cnpj' | 'cep' | 'phone' | 'opercase';
 export const stepItemAttrMaskType : Record<StepItemAttrMaskType,string> = {
   email: 'E-mail',
   number: 'Número',
@@ -26,6 +26,7 @@ export const stepItemAttrMaskType : Record<StepItemAttrMaskType,string> = {
   'cpf-cnpj': 'CPF/CNPJ',
   cep: 'CEP',
   phone: 'Telefone',
+  opercase: 'Opercase',
 };
 export type StepItemAttrMaskDynamicType = {
   type: 'number',
